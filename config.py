68675c3dbca668c0eaadccb71d10da7fd8d41523
Karthik Ayyalasomayajula <karthika0802@gmail.com>
2020-07-31 14:42:33 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = "PolCompBot"
-password = "koati88"
-client_id = "qqqG9CJvLAhaTA"
-client_secret = "3BCxyw3T2cx8xKeEMCJ-GciHaO8"
+username = ""
+password = ""
+client_id = ""
+client_secret = ""
